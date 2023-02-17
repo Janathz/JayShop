d19d0a519640a8e7f77249e284fd4b83e1be086c
Janathz <janathwijerathna@gmail.com>
2023-02-17 22:23:48 +0530

    47 User Register Reducer, Action & Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import ProductScreen from './screens/ProductScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
   return (
@@ -13,9 +14,11 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
-          <Route path='/login' component={LoginScreen} />
+
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -42,20 +42,20 @@ export const userLoginReducer = (state = {}, action) => {
   }
 };
 
-// export const userRegisterReducer = (state = {}, action) => {
-//   switch (action.type) {
-//     case USER_REGISTER_REQUEST:
-//       return { loading: true };
-//     case USER_REGISTER_SUCCESS:
-//       return { loading: false, userInfo: action.payload };
-//     case USER_REGISTER_FAIL:
-//       return { loading: false, error: action.payload };
-//     case USER_LOGOUT:
-//       return {};
-//     default:
-//       return state;
-//   }
-// };
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    // case USER_LOGOUT:
+    //   return {};
+    default:
+      return state;
+  }
+};
 
 // export const userDetailsReducer = (state = { user: {} }, action) => {
 //   switch (action.type) {
