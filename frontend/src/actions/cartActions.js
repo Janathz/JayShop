c0792c11fdfec759dda7224095fb484a19d989d7
Janathz <janathwijerathna@gmail.com>
2023-02-18 09:53:21 +0530

    Shipping Screen & Save Address

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -52,14 +52,14 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
 
-// export const saveShippingAddress = (data) => (dispatch) => {
-//   dispatch({
-//     type: CART_SAVE_SHIPPING_ADDRESS,
-//     payload: data,
-//   });
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  });
 
-//   localStorage.setItem('shippingAddress', JSON.stringify(data));
-// };
+  localStorage.setItem('shippingAddress', JSON.stringify(data));
+};
 
 // export const savePaymentMethod = (data) => (dispatch) => {
 //   dispatch({
